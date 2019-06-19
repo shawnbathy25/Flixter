27138cea8cabaddd3156c0b404726269e7a28e53
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-06-19 23:55:02 +0000

    permission

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,14 +4,9 @@ class LessonsController < ApplicationController
 
 
   def show
-   #@lesson = current_lesson.section.course(lesson_params)
-   #redirect_to instructor_course_path(current_lesson.section.course)
-   #if current_user.enrolled_in?
-   #   redirect_to lesson_path(@course)
-   # else
-   #   redirect_to current_course
-     # end
+   
   end
+
    helper_method :current_lesson
     def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
