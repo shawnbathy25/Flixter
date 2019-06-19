e43b8d458f51f00b6baf74da71912ca6ce721137
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-06-19 23:36:43 +0000

    permission

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,27 +1,29 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_enrollment_for_current_section
+  before_action :require_enrollment_for_current_course, only: [:show]
 
 
-  #def show
-   # @lesson = current_lesson.section.course(lesson_params)
-   # redirect_to instructor_course_path(current_lesson.section.course)
-  #end
-
-
-  private
-
-    
-  def require_enrollment_for_current_course
-    if !current_user.enrolled_in?(current_lesson.section.course)
-      redirect_to course_path(current_lesson.section.course), alert: "Kindly enroll to view course!"
-    end
-
-  helper_method :current_lesson
-
-  def current_lesson
+  def show
+   #@lesson = current_lesson.section.course(lesson_params)
+   #redirect_to instructor_course_path(current_lesson.section.course)
+   #if current_user.enrolled_in?
+   #   redirect_to lesson_path(@course)
+   # else
+   #   redirect_to current_course
+     # end
+  end
+   helper_method :current_lesson
+    def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-end
-end
+  private
+   def require_enrollment_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "Kindly enroll to view course!"
+    end
+  end
+
+  
+  end
+ 
