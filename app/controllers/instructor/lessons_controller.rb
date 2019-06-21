0f881117ba03fb09edba565b59502f6ab9eab20f
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-06-21 15:06:53 +0000

    ordering lesson

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -8,7 +8,7 @@ class Instructor::LessonsController < ApplicationController
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(current_section)
+    redirect_to instructor_course_path(current_section.course)
   end
   
 
